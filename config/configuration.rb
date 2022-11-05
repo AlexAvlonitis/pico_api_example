21090a8f178468022c41d3a003f7262804d4ccce
Alex <avlonitis@msn.com>
2022-11-05 22:22:09 +0000

    Add logger example

--- config/configuration.rb
+++ config/configuration.rb
@@ -1,9 +1,11 @@
 PicoApi.configure do |config|
   ## the base namespace of your app
-  config.namespace       = 'PicoApiExample'
+  config.namespace = 'PicoApiExample'
 
   ## the root directory where all the business logic resides
-  config.lib_path        = 'lib/pico_api_example'
+  config.lib_path = 'lib/pico_api_example'
+
+  config.logger = Logger.new(STDOUT) if ENV['LOG_TO_STDOUT'] == 'true'
 
   ## Error mappings. exception: status_code. PicoApi serializes a json api error object
   config.errors_map = {
